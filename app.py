4d1759e5c4799c3bdcf4e6adce83f7fa3f0ada0a
mostafa <m.ossama27@gmail.com>
2022-01-04 16:16:43 +0200

    Add Security Stack and Modify VPC stack and app entry point - Security groups for Lambda and Bastion Host - Lambda IAM role - SSM Parameters

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from aws_cdk import core
 
 #from serv.serv_stack import ServStack
 from stacks.vpc_stack import VPCStack
+from stacks.security_stack import SecurityStack
 
 app = core.App()
 
 vpc_stack = VPCStack(app, 'vpc')
+security_stack = SecurityStack(app, 'security-stack', vpc=vpc_stack.vpc)
+
 app.synth()
