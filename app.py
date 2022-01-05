7a69da96bad5cc6cd4b795f8b9aaef32c0f088fd
mostafa <m.ossama27@gmail.com>
2022-01-05 12:56:48 +0200

    Add KMS construct - Create KMS key for RDS - Store KMS Key ID in SSM parameter store

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ from aws_cdk import core
 from stacks.vpc_stack import VPCStack
 from stacks.security_stack import SecurityStack
 from stacks.bastion_stack import BastionStack
+from stacks.kms_stack import KMSStack
 
 app = core.App()
 
 vpc_stack = VPCStack(app, 'vpc-stack')
 security_stack = SecurityStack(app, 'security-stack', vpc=vpc_stack.vpc)
 bastion_stack = BastionStack(app, 'bastion-stack', vpc=vpc_stack.vpc, sg=security_stack.bastion_sg)
+kms_stack = KMSStack(app, 'kms-stack')
 
 app.synth()
