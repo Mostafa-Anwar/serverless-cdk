d7de64055f4964b24b057465508d9b39352c0046
mostafa <m.ossama27@gmail.com>
2022-01-05 17:09:30 +0200

    Add S3 construct - Create a new S3 bucket for lambda - Store the bucket name in SSM parameter store

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import os
 from aws_cdk import core 
 
 #from serv.serv_stack import ServStack
-from stacks.vpc_stack import VPCStack
+from stacks.vpc_stack      import VPCStack
 from stacks.security_stack import SecurityStack
-from stacks.bastion_stack import BastionStack
-from stacks.kms_stack import KMSStack
+from stacks.bastion_stack  import BastionStack
+from stacks.kms_stack      import KMSStack
+from stacks.s3_stack       import S3Stack
 
 app = core.App()
 
@@ -15,5 +16,6 @@ vpc_stack = VPCStack(app, 'vpc-stack')
 security_stack = SecurityStack(app, 'security-stack', vpc=vpc_stack.vpc)
 bastion_stack = BastionStack(app, 'bastion-stack', vpc=vpc_stack.vpc, sg=security_stack.bastion_sg)
 kms_stack = KMSStack(app, 'kms-stack')
+s3_stack = S3Stack(app, 's3-stack')
 
 app.synth()
