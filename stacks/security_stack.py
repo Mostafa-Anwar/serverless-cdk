91ee6ff0f8f936e5ead3aaa63aadc1f3be2d55a9
mostafa <m.ossama27@gmail.com>
2022-02-07 10:41:22 +0200

    Modify security stack - Add sg rules - Add cf output for redis sg id

--- stacks/security_stack.py
+++ stacks/security_stack.py
@@ -28,6 +28,8 @@ class SecurityStack(core.Stack):
         )
 
         self.bastion_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "SSH Access")
+        self.bastion_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), "HTTPS Access")
+        self.bastion_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP Access")
 
 
 
